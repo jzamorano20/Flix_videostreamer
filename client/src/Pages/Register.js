15fd9444bc1ef34f15dec73235c615bf4fa9a1e7
DAMIAN SMITH <damiansmith@DAMIANs-Mac-mini.local>
2023-05-16 14:58:04 -0400

    made changes to App.js

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from "react";
 
 function Register(props) {
-return (
-	<form>
-		 <input
-        name="username"
-        type="text"
-        placeholder="Enter your desired username" />
-      <input
-        name="email"
-        type="email"
-        placeholder="Enter your desired email address" />
-      <input
-        name="password"
-        type="password"
-        placeholder="Enter your desired password" />
-      <button>Submit</button>
-    </form>
-  )
+  return (
+    <>
+      <form>
+        <input
+          name="username"
+          type="text"
+          placeholder="Enter your desired username"
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="Enter your desired email address"
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Enter your desired password"
+        />
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default Register;
---
+++ server/models/Video.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+const videoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  video_url: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  thumbnail_url: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  lenght: {
+    type: Number,
+  },
+  views: {
+    type: Number,
+  },
+  likes: {
+    type: Number,
+  },
+});
