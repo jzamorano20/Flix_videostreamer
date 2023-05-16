5156e4fcab4f60dd1227183fff73e68f3e7908e9
Matt <mhgarry92@gmail.com>
2023-05-15 20:33:36 -0400

    tailwind config

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ const React = require("react");
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, This is our Video APP</h1>
+		<div className="App" class="bg-slate-500">
+
+      <h1 class=" text-white">Hello, This is our Video APP</h1>
     </div>
   );
 }
