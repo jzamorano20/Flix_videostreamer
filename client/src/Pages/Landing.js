cfa10c7ca80c0b1c04ad61fbc73c2d50a159c283
Matt <mhgarry92@gmail.com>
2023-05-16 11:12:01 -0400

    frontend routes

---
+++ client/src/Pages/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div className="App" class="bg-gradient-to-r from-white-50 to-gray-500 min-h-screen grid-cols-12 gap-4">
+      <div className="col-span-2">
+        <div class="p-2">
+          <div class="bg-black-950 flex rounded-md p-6">
+            <div class="text-white-50 bg-slate-950">Hello, This is our Video APP</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
