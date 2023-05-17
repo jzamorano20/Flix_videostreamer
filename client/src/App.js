ac55b3cd28d9f6a15cd0782d9d407eff478fe67d
Matt <mhgarry92@gmail.com>
2023-05-17 10:36:02 -0400

    Login Mock

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-import Landing from "./pages/Landing";
-import Dashboard from "./pages/Dashboard";
-import Favorites from "./pages/Favorites";
-import Register from "./pages/Register";
+import Landing from "./Pages/Landing";
+import Dashboard from "./Pages/Dashboard";
+import Favorites from "./Pages/Favorites";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
 
 const client = new ApolloClient({
   uri: "http://localhost:3333/graphql",
@@ -21,6 +22,7 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/favorites" element={<Favorites />} />
             <Route path="/register" element={<Register />} />
+						<Route path="/login" element={<Login />} />
           </Routes>
         </Router>
       </ApolloProvider>
---
+++ client/src/Pages/Login.js
@@ -0,0 +1,104 @@
+// import { useMutation } from "@apollo/client";
+// import { REGISTER_USER } from "../mutations/userMutations";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import React from "react";
+
+// function Register(props) {
+//   const [username, setUsername] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const navigate = useNavigate();
+//   const [registerUser, { error, loading }] = useMutation(REGISTER_USER);
+
+//   const handleOnChange = (event) => {
+//     const { name, value } = event.target;
+
+//     if (name === "username") {
+//       setUsername(value);
+//     }
+//     if (name === "email") {
+//       setEmail(value);
+//     }
+//     if (name === "password") {
+//       setPassword(value);
+//     }
+//   };
+
+//   const handleFormSubmit = (event) => {
+//     event.preventDefault();
+//     const username = event.target.username.value;
+//     const email = event.target.email.value;
+//     const password = event.target.password.value;
+
+//     registerUser({
+//       variables: {
+//         username,
+//         email,
+//         password,
+//       },
+//     })
+//       .then((data) => {
+//         console.log(data);
+//         navigate("/dashboard");
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   };
+function Login() {
+  return (
+    <div className="flex flex-wrap justify center mt-20">
+      <div className="w-full max-w-small">
+        <form
+          className="shadow-md bg-black-300 rounded-sm px-8 pt-6 pb-8 mb-4"
+          // onSubmit={handleFormSubmit}
+        >
+          <div className="mb-5">
+           <label
+              type="email"
+              className="block text-black-950 text-sm font-bold mb-2"
+            >
+              email
+            </label>
+            <input
+              type="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              placeholder="Enter your email address"
+              // value={email}
+              // onChange={handleOnChange}
+            />
+            <label
+              type="text"
+              className="block text-black-950 text-sm font-bold mb-2"
+            >
+              password
+            </label>
+            <input
+              type="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              // value={password}
+              // onChange={handleOnChange}
+            />
+          </div>
+          <div>
+            <button
+              className="bg-black-600 hover:bg-white-50 text-white-200 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >Login
+             {/* {loading ? "Registering User..." : "Register"} */}
+            </button>
+            {/* {error && <p className="text-red-500">{error.message}</p>} */}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
