88d067070627fa5ea54ed4095f709b81634ca294
Matt <mhgarry92@gmail.com>
2023-05-17 11:34:22 -0400

    login debug

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -3,28 +3,15 @@ import { LOGIN_USER } from "../mutations/userMutations";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Login(props) {
+function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const [loginUser, { error, loading }] = useMutation(LOGIN_USER);
 
-  const handleOnChange = (event) => {
-    const { name, value } = event.target;
-
-    if (name === "email") {
-      setEmail(value);
-    }
-    if (name === "password") {
-      setPassword(value);
-    }
-  };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-
     loginUser({
       variables: {
         email,
@@ -36,23 +23,21 @@ function Login(props) {
         navigate("/dashboard");
       })
       .catch((error) => {
-        console.error(error);
+        console.error({error})
       });
   };
 
+
   return (
-    <div className="flex flex-wrap justify center mt-20">
+    <div className="flex flex-wrap justify-center mt-20">
       <div className="w-full max-w-small">
         <form
           className="shadow-md bg-black-300 rounded-sm px-8 pt-6 pb-8 mb-4"
-          // onSubmit={handleFormSubmit}
+          onSubmit={handleFormSubmit}
         >
           <div className="mb-5">
-           <label
-              type="email"
-              className="block text-black-950 text-sm font-bold mb-2"
-            >
-              email
+            <label className="block text-black-950 text-sm font-bold mb-2">
+              Email
             </label>
             <input
               type="email"
@@ -61,13 +46,11 @@ function Login(props) {
               name="email"
               placeholder="Enter your email address"
               value={email}
-              onChange={handleOnChange}
+              onChange={(e) => setEmail(e.target.value)}
             />
-            <label
-              type="text"
-              className="block text-black-950 text-sm font-bold mb-2"
-            >
-              password
+
+            <label className="block text-black-950 text-sm font-bold mb-2">
+              Password
             </label>
             <input
               type="password"
@@ -76,15 +59,15 @@ function Login(props) {
               name="password"
               placeholder="Enter your password"
               value={password}
-            	 onChange={handleOnChange}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div>
             <button
               className="bg-black-600 hover:bg-white-50 text-white-200 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
-            >Login
-             {loading ? "Logging in..." : "Login"}
+            >
+              {loading ? "Logging in..." : "Login"}
             </button>
             {error && <p className="text-red-500">{error.message}</p>}
           </div>
@@ -93,5 +76,4 @@ function Login(props) {
     </div>
   );
 }
-
 export default Login;
