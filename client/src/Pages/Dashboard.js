91f2897d428d1b734ed08a9714995e38b521db31
Matt <mhgarry92@gmail.com>
2023-05-17 11:49:23 -0400

    Dashboard mock

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+function Landing() {
+  return (
+    <div>
 
-export default Dashboard;
+
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-white-50 bg-black-950 rounded-md p-6">
+            FLIX
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="px-4 py-6 sm:px-0">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Landing;
