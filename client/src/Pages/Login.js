079142cbbad60f47dc3984b740125ec8199f258d
Matt <mhgarry92@gmail.com>
2023-05-18 18:38:30 -0400

    login+register done

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -3,12 +3,11 @@ import { LOGIN_USER } from "../mutations/userMutations";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Login(props) {
+function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const [loginUser, { error, loading }] = useMutation(LOGIN_USER);
-
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -23,57 +22,52 @@ function Login(props) {
         navigate("/dashboard");
       })
       .catch((error) => {
-        console.error({ error })
+        console.error({ error });
       });
   };
 
-
   return (
-    <div className="flex flex-wrap justify-center mt-20">
-      <div className="w-full max-w-small">
-        <form
-          className="shadow-md bg-black-300 rounded-sm px-8 pt-6 pb-8 mb-4"
-          onSubmit={handleFormSubmit}
+    <main className="mx-auto flex min-h-screen w-full items-center justify-center bg-gradient-to-tr from-black-800 to-white-50 text-white-50">
+      <section className="flex w-[30rem] flex-col space-y-10">
+        <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-gray-600">
+          <input
+            id="email"
+            name="email"
+            placeholder="Enter your email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+          />
+        </div>
+        <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-gray-600">
+          <input
+            type="password"
+            className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="transform rounded-sm bg-gray-500 py-2 font-bold duration-300 hover:bg-black-800"
+          onClick={handleFormSubmit}
         >
-          <div className="mb-5">
-            <label className="block text-black-950 text-sm font-bold mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              name="email"
-              placeholder="Enter your email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-
-            <label className="block text-black-950 text-sm font-bold mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div>
-            <button
-              className="bg-black-600 hover:bg-white-50 text-white-200 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              {loading ? "Logging in..." : "Login"}
-            </button>
-            {error && <p className="text-red-500">{error.message}</p>}
-          </div>
-        </form>
-      </div>
-    </div>
+          {loading ? "Logging in..." : "Login"}
+        </button>
+        {error && <p className="text-red-500">{error.message}</p>}
+        <p class="text-center text-lg">
+          No account?
+          <a href="#" className="font-medium text-white-50 underline-offset-4 hover:underline px-1">
+            Create One
+          </a>
+        </p>
+      </section>
+    </main>
   );
 }
+
 export default Login;
--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -3,7 +3,7 @@ import { REGISTER_USER } from "../mutations/userMutations";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Register(props) {
+function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -26,9 +26,6 @@ function Register(props) {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    const username = event.target.username.value;
-    const email = event.target.email.value;
-    const password = event.target.password.value;
 
     registerUser({
       variables: {
@@ -47,72 +44,50 @@ function Register(props) {
   };
 
   return (
-    <div className="flex flex-wrap justify center mt-20">
-      <div className="w-full max-w-small">
-        <form
-          className="shadow-md bg-black-300 rounded-sm px-8 pt-6 pb-8 mb-4"
-          onSubmit={handleFormSubmit}
+    <main className="mx-auto flex min-h-screen w-full items-center justify-center bg-gradient-to-tr from-black-800 to-white-50 text-white-50">
+      <section className="flex w-[30rem] flex-col space-y-10">
+        <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-gray-600">
+          <input
+            id="username"
+            name="username"
+            placeholder="Enter a username"
+            value={username}
+            onChange={handleOnChange}
+            className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+          />
+        </div>
+        <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-gray-600">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter an email"
+            value={email}
+            onChange={handleOnChange}
+            className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+          />
+        </div>
+        <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-gray-600">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter a password"
+            value={password}
+            onChange={handleOnChange}
+            className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+          />
+        </div>
+        <button
+          type="submit"
+          className="transform rounded-sm bg-gray-500 py-2 font-bold duration-300 hover:bg-black-800"
+          onClick={handleFormSubmit}
         >
-          <div className="mb-5">
-            <label
-              type="text"
-              className="block text-black-950 text-sm font-bold mb-2"
-            >
-              username
-            </label>
-            <input
-              type="text"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              name="username"
-              placeholder="Enter a username"
-              value={username}
-              onChange={handleOnChange}
-            />
-
-            <label
-              type="email"
-              className="block text-black-950 text-sm font-bold mb-2"
-            >
-              email
-            </label>
-            <input
-              type="email"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              name="email"
-              placeholder="Enter a email"
-              value={email}
-              onChange={handleOnChange}
-            />
-            <label
-              type="text"
-              className="block text-black-950 text-sm font-bold mb-2"
-            >
-              password
-            </label>
-            <input
-              type="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              name="password"
-              placeholder="Enter a password"
-              value={password}
-              onChange={handleOnChange}
-            />
-          </div>
-          <div>
-            <button
-              className="bg-black-600 hover:bg-white-50 text-white-200 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              {loading ? "Registering User..." : "Register"}
-            </button>
-            {error && <p className="text-red-500">{error.message}</p>}
-          </div>
-        </form>
-      </div>
-    </div>
+          {loading ? "Registering User..." : "Register"}
+        </button>
+        {error && <p className="text-red-500">{error.message}</p>}
+      </section>
+    </main>
   );
 }
 
